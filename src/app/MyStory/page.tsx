b28e357e067ec2b6f581cf0cb71c59da5ccdb03f
DarrelG <gautanadarrel06@gmail.com>
2025-08-31 15:27:29 +0700

    Adding About me page

---
+++ src/app/MyStory/page.tsx
@@ -0,0 +1,32 @@
+const MyStory = () =>{
+    return(
+        <div>
+            <div className="mainContainer p-20 aboutme">
+                <div className="text-gray-500 ml-40 text-2xl w-1/2">
+                    <div className="text-3xl text-white font-bold">About Me</div><br />
+                    <div>— Who am i?</div><br /><br />
+                    <div className="text-justify text-xl">
+                        Hey there, im a Software Engineer and Developer. Currently study at Bina Nusantara University, majoring
+                        Computer Science focusing on Software Engineering. I believe that technology is always evolving so 
+                        we must keep learning to keep up with it. Becoming a developer is certainly not just about learning 
+                        from books or lectures, let&apos;s learn together by connecting with each other and sharing experiences.
+                    </div>
+                    <br />
+                    <div>
+                        <a href="./Darrel's Resume.pdf" download>
+                            <button className="relative flex items-center px-6 py-3 overflow-hidden font-normal text-lg transition-all bg-indigo-500 rounded-md group hover:cursor-pointer">
+                                <span className="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-indigo-700 rounded group-hover:-mr-4 group-hover:-mt-4">
+                                    <span className="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white" />
+                                </span>
+                                <span className="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full bg-indigo-600 rounded-md group-hover:translate-x-0" />
+                                <span className="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-white">Download CV</span>
+                            </button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MyStory;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,35 @@
-/* eslint-disable @next/next/no-page-custom-font */
-import type { Metadata } from "next";
-import "./globals.css";
+'use client';
 
-export const metadata: Metadata = {
-  title: "Darrel's Portfolio",
-};
+import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import './globals.css';
 
-export default function RootLayout({
-children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const [animateToCenter, setAnimateToCenter] = useState(false);
+
+  useEffect(() => {
+    if (pathname === '/MyStory') {
+      setAnimateToCenter(true);
+    } else {
+      setAnimateToCenter(false);
+    }
+  }, [pathname]);
+
   return (
     <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet"></link>
-      </head>
       <body>
+        <div className={`fixed z-50 transition-all duration-700 
+        ${animateToCenter ? 'top-30 p-20 ml-40 font-normal text-3xl text-white' : 'text-white text-7xl top-0 left-0 p-20 ml-40 opacity-0 font-bold w-1/2'}`}>
+          <div className="relative h-max mb-10">
+            <div className={`absolute transition-all ${animateToCenter ? 'left-0 top-0 duration-1000 delay-500' : 'left-0 top-0 duration-500'}`}>
+              Darrel
+            </div>
+            <div className={`absolute transition-all ${animateToCenter ? 'left-22 top-0 opacity-100 duration-1000 delay-500' : 'left-0 top-18 opacity-500 duration-500'}`}>
+              Gautama
+            </div>
+          </div>
+      </div>
         {children}
       </body>
     </html>
